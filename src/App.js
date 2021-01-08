a6c82708d8d40c06ca3c9a03cd6c91919244c6df
osalam <tangbinhlam@gmail.com>
2021-01-08 12:09:07 -0800

    create simple component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <section className='book-list'>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
 }
 
+const Book = () => {
+  return <article className='book'>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+}
+
+const Image = () => {
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg" alt="Untamed" />
+}
+
+const Title = () => {
+  return <h1>If Animals Kissed Good Night</h1>
+}
+const Author = () => {
+  return <h4>Ann Whitford Paul</h4>
+}
 export default App;
