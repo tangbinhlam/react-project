61b78831065cf320c4da3e9f858631cd758ab492
osalam <tangbinhlam@gmail.com>
2021-01-08 15:16:45 -0800

    Refactor to move books compent to new component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
 import './App.css';
-const title = 'If Animals Kissed Good Night'
-const author = 'Ann Whitford Paul'
-const image_url = 'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg'
-
-const books = [
-  {
-    image_url: 'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg',
-    title: 'If Animals Kissed Good Night',
-    author: 'Ann Whitford Paul',
-    cost: 4.79,
-  },
-  {
-    image_url: 'https://images-na.ssl-images-amazon.com/images/I/910lu-HKDYL._AC_UL200_SR200,200_.jpg',
-    title: 'Little Blue Truck\'s Valentine',
-    author: 'Alice Schertle',
-    cost: 12.19,
-  },
-  {
-    image_url: 'https://images-na.ssl-images-amazon.com/images/I/A1elFK9VYEL._AC_UL200_SR200,200_.jpg',
-    title: 'Bronco and Friends: A Party to Remember',
-    author: 'Tim Teblow',
-    cost: 15.22,
-  }
-]
+import Books from './components/books/Books';
 
 function App() {
-  return <section className='book-list'>{books.map(book => {
-    return <Book book={book} />
-  })}
-  </section>
-}
-
-const Book = (props) => {
-  console.log(props);
-  const { image_url, title, author, cost, children } = props.book
-  return <article className='book'>
-    <img src={image_url} alt={title} />
-    <h1 style={{ color: 'blue' }}>{title}</h1>
-    <h4>{author}</h4>
-    <h5>{cost}</h5>
-    <p>{children}</p>
-  </article>
+  return <Books />;
 }
 
 export default App;
---
+++ src/components/books/Books.js
@@ -0,0 +1,42 @@
+import './Books.css';
+
+const books = [
+    {
+        image_url: 'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg',
+        title: 'If Animals Kissed Good Night',
+        author: 'Ann Whitford Paul',
+        cost: 4.79,
+    },
+    {
+        image_url: 'https://images-na.ssl-images-amazon.com/images/I/910lu-HKDYL._AC_UL200_SR200,200_.jpg',
+        title: 'Little Blue Truck\'s Valentine',
+        author: 'Alice Schertle',
+        cost: 12.19,
+    },
+    {
+        image_url: 'https://images-na.ssl-images-amazon.com/images/I/A1elFK9VYEL._AC_UL200_SR200,200_.jpg',
+        title: 'Bronco and Friends: A Party to Remember',
+        author: 'Tim Teblow',
+        cost: 15.22,
+    }
+]
+
+function Books() {
+    return <section className='book-list'>{books.map(book => {
+        return <Book book={book} />
+    })}
+    </section>
+}
+
+const Book = (props) => {
+    const { image_url, title, author, cost, children } = props.book
+    return <article className='book'>
+        <img src={image_url} alt={title} />
+        <h1 style={{ color: 'blue' }}>{title}</h1>
+        <h4>{author}</h4>
+        <h5>{cost}</h5>
+        <p>{children}</p>
+    </article>
+}
+
+export default Books;
